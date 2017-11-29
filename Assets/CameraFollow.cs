590985792e09cdee07150e4e4eb5099dd20c9524
Alexandre Demode <contact@alex-d.fr>
2017-11-29 14:51:42 +0100

    Animations and controls

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraFollow : MonoBehaviour {
+
+    public Player player;
+    public Vector2 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// LateUpdate is called once per frame after all Updates
+	void LateUpdate () {
+        var playerPosition = player.transform.position;
+        var destination = new Vector3(
+            playerPosition.x + offset.x,
+            playerPosition.y + offset.y,
+            transform.position.z
+        );
+        transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime * 8f);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 5f;
+        transform.Translate(x, 0f, 0f, Space.World);
+
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        var r2d = GetComponent<Rigidbody2D>();
+
+        if (Mathf.Abs(Mathf.Sign(r2d.velocity.x) - Mathf.Sign(x)) > 0.1f)
+        {
+            r2d.velocity = new Vector2(0f, r2d.velocity.y);
+        }
+
+        if (!Mathf.Approximately(x, 0))
+        {
+            spriteRenderer.flipX = x > 0;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            r2d.velocity = new Vector2(r2d.velocity.x, 5f);
+            GetComponent<Animator>().SetTrigger("Jump");
+        }
+	}
+}
